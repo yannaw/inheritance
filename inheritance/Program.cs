94f9c9ab336bedd70714643e0536d6fb70308842
yannaw <yannaw@hotmail.com>
2017-05-30 15:33:20 +0200

    Add project files.

---
+++ inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace inheritance {
+    class Program {
+        static void Main(string[] args) {
+            AClass a = new AClass(4);
+            BClass b = new BClass();
+            AClass ab = b;// new BClass(); // Den mest specialicerade kommer att finnas i AClass om metoder är hidden
+            AbstractClass ac = b; //ger endast AbstractClassens metoder
+
+            WriteLine("1");
+            WriteLine(a.Override());
+            WriteLine(b.Override());
+            WriteLine(ab.Override()); 
+
+            WriteLine("\n2");
+            WriteLine(a.Hidden());
+            WriteLine(b.Hidden());
+            WriteLine(ab.Hidden());
+
+            WriteLine("\n3");
+            WriteLine(a.Abstract());
+            WriteLine(b.Abstract());
+            WriteLine(ab.Abstract());
+
+            WriteLine("\n4");
+            WriteLine(a.Concrete());
+            WriteLine(b.Concrete());
+
+            WriteLine("\n5");
+            WriteLine(a.InterfaceString1());
+            WriteLine(b.InterfaceString2());
+            WriteLine((ab as BClass).InterfaceString2());
+
+            WriteLine("\n6");
+            WriteLine(a.InterfaceString1());
+            WriteLine(b.InterfaceString1());
+            WriteLine(ab.InterfaceString1());
+            WriteLine((b as Interface2).InterfaceString1());
+
+
+            WriteLine("\n");
+            WriteLine("\n");
+
+        }
+    }
+}
---
+++ inheritance/classes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance {//Så länge som funktioner är abstrakta är det inga problem med att ärva+implementera flera klasser med samma funktionsnamn
+
+    interface Interface1 {
+        string InterfaceString1();
+    }
+
+    interface Interface2 {
+        int InterfaceString1();
+        string InterfaceString2();
+    }
+
+    abstract class AbstractClass {
+        public int AFlag { get; set; }
+        public AbstractClass() { AFlag = 1; }
+        public AbstractClass(int flag ) { AFlag = flag; }
+
+        public abstract string Abstract();
+        public virtual string Concrete() => "Abstract:Concrete";
+    }
+
+    class AClass : AbstractClass, Interface1{
+//        public AClass() { }
+        public AClass(int flag):base(flag) { }//base = refererar basklassens konstruktor här anropas den 3:e konstruktorn
+        public virtual string Override()=>"A:Override";
+        public string Hidden() => "A:Hidden";
+        public override string Abstract() => "A:Abstract()";
+        public string InterfaceString1() =>"A:InterfaceString1";
+    }
+
+    class BClass :AClass, Interface2 {
+        public BClass():base(3){ }//base = refererar basklassens konstruktor
+
+        public override string Override() => "B:Override"; //expressionsyntax, ekvivalent med måsvingar+ return
+        public new string Hidden() => "B:Hidden"; //new när det är medvetet hidden annars virtual/override
+        public override string Concrete() => "B:Concrete";
+        public string InterfaceString2() => "B:InterfaceString2";
+        int Interface2.InterfaceString1() => 2; //explisiv interfaceimplementation
+
+    }
+}
